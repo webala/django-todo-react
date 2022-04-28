69e44c260a545106e9b64b6c1aad7bfa34334ab1
webala <webala1001@gmail.com>
2022-04-28 07:40:16 +0300

    app complete

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,9 +19,9 @@ from rest_framework import routers
 from todo import views
 
 router = routers.DefaultRouter()
-router.register(r'todos', views.Todoview)
+router.register(r'todos', views.TodoView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(routers.urls))
+    path('api/', include(router.urls))
 ]
